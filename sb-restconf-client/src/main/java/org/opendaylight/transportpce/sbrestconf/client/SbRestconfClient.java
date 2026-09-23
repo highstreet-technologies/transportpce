580704a2bdc8887dccfe4384478b0158c272a72b
Michael Dürre <michael.duerre@highstreet-technologies.com>
2026-09-23 06:01:51 +0200

    add seperate exception handling for responses

--- sb-restconf-client/src/main/java/org/opendaylight/transportpce/sbrestconf/client/SbRestconfClient.java
+++ sb-restconf-client/src/main/java/org/opendaylight/transportpce/sbrestconf/client/SbRestconfClient.java
@@ -17,6 +17,7 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.ResponseProcessingException;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -205,10 +206,15 @@ public class SbRestconfClient implements AutoCloseable {
                 LOG.error("{} {} returned HTTP {}: {}", method, url, response.getStatus(), body);
                 return false;
             }
-        } catch (Exception e) {
-            LOG.error("{} {} failed", method, url, e);
-            return false;
         }
+        catch (ResponseProcessingException e){
+            LOG.error("{} {} failed with status {}:", method, url, e.getResponse().getStatus(), e.fillInStackTrace());
+        }
+        catch (Exception e) {
+            LOG.error("{} {} failed", method, url, e);
+
+        }
+        return false;
     }
 
     /**
